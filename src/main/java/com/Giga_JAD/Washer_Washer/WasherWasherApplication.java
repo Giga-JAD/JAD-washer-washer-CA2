40394c872753e8f98f4188806222425293017171
ProjektKevin <minthetkhine05@gmail.com>
2025-01-23 18:54:11 +0800

    Project Set up

---
+++ src/main/java/com/Giga_JAD/Washer_Washer/WasherWasherApplication.java
@@ -0,0 +1,13 @@
+package com.Giga_JAD.Washer_Washer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasherWasherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasherWasherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Giga_JAD/Washer_Washer/WasherWasherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Giga_JAD.Washer_Washer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasherWasherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
